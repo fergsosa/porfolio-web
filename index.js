4b315fffc9ddfc90d2851b90e84e69e8e8e4df19
fergsosa <134444526+fergsosa@users.noreply.github.com>
2024-01-22 21:51:49 -0300

    JS modo dark/scroll espia/ valiadacion form

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import toggleTheme from "./js/toggleTheme.js";
 import scrollSpy from "./js/scrollEspia.js";
+import contactFormValidations from "./js/validacionForm.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
-  console.log("Hola Mundo");
   scrollSpy();
+  contactFormValidations();
 });
 
 // toggleTheme(".btn-theme-dark", "dark-mode");
